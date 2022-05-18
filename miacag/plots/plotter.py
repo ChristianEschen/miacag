0475693a0400521fbee9727bd9097cf29f5937cc
ChristianEschen <christian_eschen@hotmail.com>
2022-05-18 15:23:59 +0200

    fix plotting

--- miacag/plots/plotter.py
+++ miacag/plots/plotter.py
@@ -47,7 +47,7 @@ def create_empty_csv():
 
 
 def getNormConfMat(df, labels_col, preds_col,
-                   plot_name, f1, output, num_classes):
+                   plot_name, f1, output, num_classes, support):
     labels = [i for i in range(0, num_classes)]
     conf_arr = confusion_matrix(df[labels_col], df[preds_col], labels=labels)
     sum = conf_arr.sum()
@@ -64,11 +64,20 @@ def getNormConfMat(df, labels_col, preds_col,
     ax.set_aspect(1)
     cmap = sns.cubehelix_palette(light=1, as_cmap=True)
     res = sns.heatmap(df_cm, annot=True, vmin=0.0, vmax=100.0, fmt='.2f',
+                      square=True, linewidths=0.1, annot_kws={"size": 8},
                       cmap=cmap)
     res.invert_yaxis()
     f1 = np.round(f1, 3)
-    plt.title(plot_name + ': Confusion Matrix, F1-macro:' + str(f1))
+    plt.title(
+        plot_name + ': Confusion Matrix, F1-macro:' + str(f1))
     plt.savefig(os.path.join(output, plot_name + '_cmat.png'), dpi=100,
+                bbox_inches='tight')
+    plt.close()
+
+    plt.title(
+        plot_name + ': Confusion Matrix, F1-macro:' + str(f1) +
+        ',support(N)=' + str(support))
+    plt.savefig(os.path.join(output, plot_name + '_cmat_support.png'), dpi=100,
                 bbox_inches='tight')
     plt.close()
     return None
@@ -77,24 +86,26 @@ def getNormConfMat(df, labels_col, preds_col,
 def plot_results(sql_config, output_plots, num_classes, roc=False):
     df, _ = getDataFromDatabase(sql_config)
     for label_name in sql_config['labels_names']:
-        df = df[df[label_name].notna()]
-        df[label_name + '_predictions'] = \
-            df[label_name + '_predictions'].astype(float).astype(int)
-        df[label_name] = df[label_name] \
+        df_label = df[df[label_name].notna()]
+        df_label[label_name + '_predictions'] = \
+            df_label[label_name + '_predictions'].astype(float).astype(int)
+        df_label[label_name] = df_label[label_name] \
             .astype(float).astype(int)
         f1_transformed = f1_score(
-            df[label_name],
-            df[label_name + '_predictions'],
+            df_label[label_name],
+            df_label[label_name + '_predictions'],
             average='macro')
+        support = len(df_label)
 
         getNormConfMat(
-            df,
+            df_label,
             label_name,
             label_name + '_predictions',
             label_name,
             f1_transformed,
             output_plots,
-            num_classes)
+            num_classes,
+            support)
         df = df.replace(
             {label_name: map_1abels_to_0neTohree()})
         df = df.replace(
@@ -102,11 +113,11 @@ def plot_results(sql_config, output_plots, num_classes, roc=False):
         f1 = f1_score(df[label_name],
                       df[label_name + '_predictions'], average='macro')
         getNormConfMat(df, label_name, label_name + '_predictions',
-                       'labels_3_classes', f1, output_plots, 3)
+                       'labels_3_classes', f1, output_plots, 3, support)
 
         if roc is True:
             plot_roc_curve(df[label_name], df[label_name + '_confidences'],
-                           output_plots, label_name)
+                           output_plots, label_name, support)
     return None
 
 
@@ -117,7 +128,7 @@ def convertConfFloats(confidences):
     return np.array(confidences_conv)
 
 
-def plot_roc_curve(labels, confidences, output_plots, plot_name):
+def plot_roc_curve(labels, confidences, output_plots, plot_name, support):
     labels = labels.to_numpy()
     confidences = convertConfFloats(confidences)
     fpr, tpr, thresholds = metrics.roc_curve(labels, confidences, pos_label=1)
@@ -131,9 +142,25 @@ def plot_roc_curve(labels, confidences, output_plots, plot_name):
     plt.xlim([0, 1.05])
     plt.ylim([0, 1.05])
     plt.ylabel('True Positive Rate (Sensitivity)')
-    plt.xlabel('False Positive Rate (1 - Specificity')
+    plt.xlabel('False Positive Rate (1 - Specificity)')
     plt.show()
     plt.savefig(os.path.join(output_plots, plot_name + '_roc.png'), dpi=100,
+                bbox_inches='tight')
+    plt.close()
+
+    plt.clf()
+    plt.figure()
+    plt.title('Receiver Operating Characteristic, support(N):' + str(support))
+    plt.plot(fpr, tpr, 'b', lw=2, label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1.05])
+    plt.ylim([0, 1.05])
+    plt.ylabel('True Positive Rate (Sensitivity)')
+    plt.xlabel('False Positive Rate (1 - Specificity)')
+    plt.show()
+    plt.savefig(os.path.join(
+        output_plots, plot_name + '_roc_support.png'), dpi=100,
                 bbox_inches='tight')
     plt.close()
 
